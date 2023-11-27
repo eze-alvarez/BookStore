032099833dbb470794a0bdb90d242e16c80ab36b
eze-alvarez <ezenigm@hotmail.com>
2023-11-27 13:57:36 -0300

    layout problem

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
 import Book from '../components/Book'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { useAppContext } from '../store/Store'
 import styles from './Index.module.css'
 // import { data } from '../api/api'
